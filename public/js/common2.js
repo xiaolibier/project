fd0a697124ce3e96b6c42f8392e371404b9b632c
lixianqi <1350461875@qq.com>
2018-03-07 10:10:17 +0800

    11

--- public/js/common2.js
+++ public/js/common2.js
@@ -444,6 +444,7 @@ $(function(){
 					var sourceLink2 = sourceLink; //pdf文件
 					if(sourceLink2.indexOf('.pdf') > -1){sourceLink2="../public/img/pdf.png";}
 					$('#'+bid).attr('src',sourceLink);
+					$('#'+bid).siblings('.pdf_file_name').html(res.key).attr('href',sourceLink);
 					$('#'+bid).siblings('.head_imgs').css({'background':'url('+sourceLink2+') no-repeat center center','background-size':'contain'});
 					$('#'+bid).parents('.head_img_spam').addClass('active');
 					$('#'+bid).siblings('.hide_head_imgs').css({'background':'url('+sourceLink+') no-repeat center center','background-size':'contain'});
